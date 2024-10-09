83f291ed618d3d289b7951b44bc7136ee93f9d7f
Koen Eelen <koen.eelenpubliq.be>
2024-10-09 20:04:42 +0200

    push

--- src/Controller/OverviewController.php
+++ src/Controller/OverviewController.php
@@ -4,22 +4,26 @@ declare(strict_types=1);
 
 namespace Src\Controller;
 
+use Src\Model\Book;
 use Twig\Environment;
+use PDO;
 
 class OverviewController implements HtmlControllerInterface
 {
     private Environment $twig;
+    private PDO $pdo;
 
-    public function __construct(Environment $twig)
+    public function __construct(Environment $twig, Pdo $pdo)
     {
         $this->twig = $twig;
+        $this->pdo = $pdo;
     }
 
     public function render(): void
     {
-        //@todo finish code
+        $books = Book::selectAll($this->pdo);
 
         $template = $this->twig->load('overview.html.twig');
-        $template->display();
+        $template->display(['books' => $books]);
     }
 }
